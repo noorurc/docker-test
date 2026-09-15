f93f53828d46170d053e87912104531894289ead
noorurc <noorur.c@googlemail.com>
2026-09-15 14:19:29 +0100

    Create script1.py

---
+++ script1.py
@@ -0,0 +1,11 @@
+from nornir impor tInitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornor(config_file="config.yaml")
+
+def test_connect(task):
+  task.run(task=netmiko_send_command, command_string="show configuration | display set")
+
+result = nr.run(task=test_connect)
+print_result(result)
