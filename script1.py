b43691cf17ed0fd57b1f39e61dffb600a0a6784d
noorurc <noorur.c@googlemail.com>
2026-09-16 00:37:13 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -5,7 +5,7 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yaml")
 
 def test_connect(task):
-  task.run(task=netmiko_send_command, command_string="show configuration | display set")
+  task.run(task=netmiko_send_command, command_string="show configuration | display xml")
 
 result = nr.run(task=test_connect)
 print_result(result)
