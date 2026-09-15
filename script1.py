a4ee444ce6f0ee85346886aa8fbe7240b806a6c3
noorurc <noorur.c@googlemail.com>
2026-09-15 22:07:17 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,8 +1,8 @@
-from nornir impor tInitNornir
+from nornir import InitNornir
 from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
 
-nr = InitNornor(config_file="config.yaml")
+nr = InitNornir(config_file="config.yaml")
 
 def test_connect(task):
   task.run(task=netmiko_send_command, command_string="show configuration | display set")
